e0d6dc2299235497bb3008360645a58fa9aab265
Micha1207 <mlusz1601@gmail.com>
2026-04-19 16:31:52 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ * MSG - Micha1207's Snake Game.
+ *
+ * Author:  Micha1207
+ * Project: MSG (https:/github.co/Micha1207/MSG)
+ * Date:    Apr 17, 2026
+ * License: GNU GPL v3 (full license in LICENSE file)
+ * This program comes with NO WARRANTY; to the extent permitted by law.
+ */
+#include <stdio.h>
+
+#include "snake.h"
+
+/*
+ * Main function.
+ * Calls 'snake()' function with snake game's logic.
+ * Returns 0 on succes.
+ */
+int main(){
+  snake();
+
+  return 0;
+}
